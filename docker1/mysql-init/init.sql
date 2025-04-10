9b1d16ea9621ee1cd5064f7d98b15699c5e35cfa
jayashreem27 <jayashree.m270804@gmail.com>
2025-04-10 13:29:37 +0530

    grafana1

--- docker1/mysql-init/init.sql
+++ docker1/mysql-init/init.sql
@@ -3,7 +3,7 @@ USE logsdb;
 CREATE TABLE IF NOT EXISTS logs (
     id INT AUTO_INCREMENT PRIMARY KEY,
     timestamp DATETIME,
-    level VARCHAR(10),
+    level VARCHAR(50),
     message TEXT,
     service VARCHAR(50)
 );
